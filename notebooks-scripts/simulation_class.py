e681bdcc4e6692e4861336583c68d3aff8b96226
JLuijten <135598395+JLuijten@users.noreply.github.com>
2026-01-13 21:05:42 +0100

    completed step function
    vector math now works and updated al agents

--- notebooks-scripts/simulation_class.py
+++ notebooks-scripts/simulation_class.py
@@ -6,12 +6,16 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 # import our own functions
 from agent_class import Agent, Predator
+from wall import wall_vec
 class Simulation():
-    def __init__(self, N_birds, nearest_x):
+    def __init__(self, N_birds, nearest_x, loc_vector_scale, dir_vector_scale, noise_vector_scale):
         # needed variables
         self.timestep = 0
         self.N_birds = N_birds
         self.nearest_x = nearest_x
+        self.loc_vector_scale = loc_vector_scale
+        self.dir_vector_scale = dir_vector_scale
+        self.noise_vector_scale = noise_vector_scale
 
         # initialize birds in a list
         self.agents = [Agent(i) for i in range(N_birds)]
@@ -41,10 +45,37 @@ class Simulation():
             nearest_ids = self.nearest_x_ids(i,j,k,ids,self.nearest_x,id)
 
             # three components of speed vector: location, direction, noise ; n = neighbour
-            total_loc_vector = [0, 0, 0] #x, y, z
-            total_direction_vector = [0, 0, 0] #x, y, z
+            total_loc_vector = np.array([0, 0, 0]) #x, y, z
+            total_direction_vector = np.array([0, 0, 0]) #x, y, z
             for id in nearest_ids:
                 nx, ny, nz, nvx, nvy, nvz, nid = self.agents[id].output_last()
+                total_loc_vector += [nx - x, ny - y, nz - z]
+                total_loc_vector += [nvx, nvy, nvz]
+            loc_vec = x / self.nearest_x
+            direction_vec = x / self.nearest_x
+
+            # these vectors need to be normalised and given their required scale afctor
+            scaled_loc_vec = (loc_vec / np.linalg.norm(loc_vec)) * self.loc_vector_scale 
+            scaled_dir_vec = (direction_vec / np.linalg.norm(direction_vec)) * self.dir_vector_scale
+            # noise vector
+            noise = np.random.normal(size=3)
+            scaled_noise = noise / np.linalg.norm(noise) * self.noise_vector_scale
+
+            #total movement :
+            total_vec = scaled_loc_vec + scaled_dir_vec + scaled_noise
+
+            ##### all other influences on movement (wall and wind)
+            wall = np.array(wall_vec(x, y, z, 5))
+
+            total_vec += wall 
+
+            # assign new loc and speed to agent
+            agent.set_current(x + total_vec[0], y + total_vec[1], z + total_vec[2], total_vec[0], total_vec[1], total_vec[2])
+        
+        # all agents now calculated a new location and speed -> step calculations done -> "current" to "last" for next step
+        for agent in self.agents:
+            agent.current_to_last()
+
 
     def show(self):
         i, j, k = [], [], []
@@ -66,12 +97,15 @@ class Simulation():
         ax.set_ylabel("y")
         ax.set_zlabel("z")
 
-        plt.show()
+        plt.show(block=False)   # non-blocking
+        plt.pause(0.2)          # keep the window open for 0.2s
+        plt.close()
+
     def dump():
         return
     
 
-    def nearest_x_ids(i, j, k, ids, near_x, initial_bird):
+    def nearest_x_ids(self, i, j, k, ids, near_x, initial_bird):
         n = initial_bird
         nr = len(i)
         items = []  # (distance, id)
@@ -89,5 +123,6 @@ class Simulation():
 
         return [id_ for _, id_ in smallest_near_x]
 
-sim = Simulation(100, 7)
+sim = Simulation(100, 7, 0.45, 0.45, 0.1)
+sim.step()
 sim.show()
