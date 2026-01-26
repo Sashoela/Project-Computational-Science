545833f30dd297155d4da4b0c8a0515a383ea074
JLuijten <135598395+JLuijten@users.noreply.github.com>
2026-01-26 21:08:12 +0100

    concave hull and multiple simulations

--- notebooks-scripts/validation.py
+++ notebooks-scripts/validation.py
@@ -1,60 +1,66 @@
 from simulation_class import Simulation
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy.spatial import ConvexHull
+from scipy import stats
+import alphashape
+import trimesh
+from sklearn.neighbors import NearestNeighbors
 
-#run simulation
-sim = Simulation(200, 7, 0.3, 0.35, 0.25, 0.1, 600, 600)
-for _ in range(150):
-    sim.step()
-x, y, z = sim.dump()
-
-
-# build hull
-points = np.column_stack((x, y, z))
-hull = ConvexHull(points)
-
-# fuction that calculates distance to all hull planes and returnes smallest
-def distance_to_hull(point, hull):
-    return min(
-        abs(eq[:3] @ point + eq[3]) / np.linalg.norm(eq[:3])
-        for eq in hull.equations
-    )
-
-
-# #center of mass:
-# avg_x = (max(x) + min(x)) / 2
-# avg_y = (max(y) + min(y)) / 2
-# avg_z = (max(z) + min(z)) / 2
+def auto_alpha(points, k=6, scale=1.5):
+    nbrs = NearestNeighbors(n_neighbors=k+1).fit(points)
+    dists, _ = nbrs.kneighbors(points)
+    mean_nn_dist = dists[:,1:].mean()  # skip self distance
+    return scale / mean_nn_dist
 
 #check distance to border and density for each agent for a number of simulations
 dist_to_border = []
 density = []
 
+for i in range(10):
+    #run simulation
+    x, y, z = 0, 0, 0
+    sim = Simulation(200, 7, 0.3, 0.35, 0.25, 0.1, 600, 600)
+    for _ in range(150):
+        sim.step()
+    x, y, z = sim.dump()
 
-for n in range(len(x)): # n is current bird
-    #check against others
-    dens = 0
-    for m in range(len(x)):
-        if n != m:
-            d = np.sqrt(
-                (x[n] - x[m])**2 +
-                (y[n] - y[m])**2 +
-                (z[n] - z[m])**2
-            )
-            if d <= 2:
-                dens += 1
-    density.append(dens)
-    #dist to border
-    to_border = distance_to_hull(points[n], hull)
 
-    dist_to_border.append(to_border)
+    # # build hull
+    points = np.column_stack((x, y, z))
+    # hull = ConvexHull(points)
+
+    alpha = auto_alpha(points)
+    shape = alphashape.alphashape(points, alpha)
+    mesh = trimesh.Trimesh(vertices=shape.vertices, faces=shape.faces)
+    #distances to mesh
+    distances = np.abs(mesh.nearest.signed_distance(points))
+
+    for n in range(len(x)): # n is current bird
+        #check against others
+        dens = 0
+        for m in range(len(x)):
+            if n != m:
+                d = np.sqrt(
+                    (x[n] - x[m])**2 +
+                    (y[n] - y[m])**2 +
+                    (z[n] - z[m])**2
+                )
+                if d <= 2:
+                    dens += 1
+        density.append(dens)
+        dist_to_border.append(distances[n])
 
 #plot and fit
-a, b = np.polyfit(dist_to_border, density, 1)
-plt.scatter(dist_to_border, density)
-plt.plot(dist_to_border, a * np.array(dist_to_border) + b, 'r-', label=f"best fit, a = {a}")
+best_fit = stats.linregress(dist_to_border, density)
+# plt.scatter(dist_to_border, density, alpha = 0.2)
+plt.hexbin(dist_to_border, density, gridsize=50, cmap="viridis")
+plt.colorbar(label="count")
+plt.plot(dist_to_border, best_fit.slope * np.array(dist_to_border) + best_fit.intercept, 'r-', label=f"best fit, a = {best_fit.slope}")
 plt.xlabel("distance to border")
 plt.ylabel("local density")
+plt.ylim(0, max(density))
 plt.legend(loc = "best")
+plt.title(f"r^2 = {best_fit.rvalue**2}")
 plt.show()
+
+plt.savefig("density_validation2.png")
